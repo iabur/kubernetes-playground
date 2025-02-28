2d80ea46ee4ed771050c45b11500dc769778ed62
iabur <iaburrahman@brainstation-23.com>
2025-02-28 17:25:08 +0600

    initial commit

---
+++ src/test/java/org/test/jenkinsspringboot/JenkinsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.test.jenkinsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
